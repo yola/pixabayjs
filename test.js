9836e0e50979d30123f53e46656ebf3e5984da94
Jacob <jacob@yola.com>
2015-04-30 15:13:30 -0700

    Setup tests

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('pixabayjs', function() {
+  it('should pass this test', function() {
+    expect(true).to.be.true;
+  });
+});
