887ed423cd015364424fc5a80d17d69c0dd3fded
Jacob <jacob@yola.com>
2015-06-22 16:13:24 -0700

    Use mockagent in tests

--- test.js
+++ test.js
@@ -1,21 +1,25 @@
 'use strict';
 
-var assign = require('lodash.assign');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
-var config = require('./config.json');
+var fill = require('lodash.fill');
+var mockagent = require('mockagent');
 var pixabay = require('./src/index');
 var promiseHelpers = require('promisehelpers');
+var querystring = require('querystring');
+var superagent = require('superagent');
+var url = require('url');
 
 var notify = promiseHelpers.notify;
-var wrap = promiseHelpers.wrap;
 
 var expect = chai.expect;
 chai.use(chaiAsPromised);
 chai.should();
 
-var username = config.pixabay.username;
-var key = config.pixabay.api_key;
+var username = 'username';
+var key = 'key';
+
+var pixabayUrl = 'https://pixabay.com/api/';
 
 describe('pixabayjs', function() {
   this.timeout(30000);
@@ -50,6 +54,33 @@ describe('pixabayjs', function() {
     var query;
 
     before(function(done) {
+      mockagent.target(superagent);
+
+      mockagent.url(pixabayUrl, function(res) {
+        var query = url.parse(this.url).query;
+        var queryParams = querystring.parse(query);
+
+        var firstPage = queryParams.page !== '2';
+        var emptyArr =  firstPage ? new Array(20) : new Array(5);
+        fill(emptyArr, 1);
+
+        var hits = emptyArr.map(function(v, i) {
+          var offset = firstPage ? 0 : 20;
+          return offset + i + 1;
+        });
+
+
+        var response = {
+          status: 200,
+          body: {
+            totalHits: 25,
+            hits: hits
+          }
+        };
+
+        return response;
+      });
+
       query = {
         order: 'lastest'
       };
@@ -57,24 +88,28 @@ describe('pixabayjs', function() {
       client = Object.create(pixabay);
       client.authenticate(username, key);
 
-      client
-        .request()
-        .query(query)
+      request = client
+        .request();
+
+
+      request.query(query)
         .search(['dog', 'puppy'])
         .get()
-        .then(wrap(request, 'data'))
         .done(notify(done));
     });
 
-    it('hits the api', function() {
-      expect(request.data.status).to.equal(200);
+    after(function() {
+      mockagent.releaseTarget();
     });
 
-    it('sends the query parameters', function() {
-      var queries = assign(query,
-        {username: username, key: key},
-        {q: 'dog+puppy'}
-      );
+    it('hits the api', function() {
+      expect(request.response.status).to.equal(200);
+    });
+
+    it('receives hits', function() {
+      expect(request.data.totalHits).to.equal(25);
+      expect(request.hits).to.be.length(20);
+    });
 
       expect(request.data.request.qs).to.eql(queries);
     });
