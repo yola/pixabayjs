331d58042cf0a531465202d35555a0a7932b8067
Jacob <jacob@yola.com>
2015-09-02 23:11:34 -0700

    ES6ify index.js

--- test/pixabay-spec.js
+++ test/pixabay-spec.js
@@ -1,19 +1,20 @@
 'use strict';
 
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var pixabay = require('../src/index');
-var ResultList = require('../src/result-list');
+import ResultList from '../src/result-list';
 
-var expect = chai.expect;
+let chai = require('chai');
+let chaiAsPromised = require('chai-as-promised');
+let pixabay = require('../src/index');
+
+let expect = chai.expect;
 chai.use(chaiAsPromised);
 
-var username = 'username';
-var key = 'key';
+let username = 'username';
+let key = 'key';
 
 describe('Pixabayjs', function() {
   this.timeout(30000);
-  var client;
+  let client;
 
   beforeEach(function() {
     client = Object.create(pixabay);
@@ -32,7 +33,7 @@ describe('Pixabayjs', function() {
   });
 
   describe('resultList', function() {
-    var search = ['dogs', 'puppies'];
+    let search = ['dogs', 'puppies'];
 
     it('returns a ResultList', function() {
       expect(client.resultList(search)).to.be.instanceof(ResultList);
