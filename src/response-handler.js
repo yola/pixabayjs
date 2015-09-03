d988f56ed625abed2150d678ff19e4acaa02b460
Jacob <jacob@yola.com>
2015-09-02 23:10:54 -0700

    ES6ify src/response-handler.js

--- src/response-handler.js
+++ src/response-handler.js
@@ -1,38 +1,41 @@
 'use strict';
 
-var q = require('q');
+let q = require('q');
 
-var noop = function(res) {
+let noop = function(res) {
   return res;
 };
 
-function ResponseHandler(res, page, perPage, cb) {
-  this.res = res;
-  this.page = page;
-  this.perPage = perPage;
-  this.cb = cb || noop;
+class ResponseHandler {
+  constructor(res, page, perPage, cb) {
+    this.res = res;
+    this.page = page;
+    this.perPage = perPage;
+    this.cb = cb || noop;
+  }
+
+  success() {
+    let data = JSON.parse(this.res.xhr.responseText);
+    data.page = this.page;
+    data.totalPages = Math.ceil(data.totalHits / this.perPage);
+    data.error = null;
+
+    return q(this.cb(data));
+  }
+
+  failure() {
+    let error = JSON.parse(this.res.response.xhr.responseText);
+    let data = {
+      error: error,
+      page: this.page,
+      hits: [],
+      totalHits: null,
+      totalPages: null
+    };
+
+    return q(this.cb(data));
+  }
 }
 
-ResponseHandler.prototype.success = function() {
-  var data = JSON.parse(this.res.xhr.responseText);
-  data.page = this.page;
-  data.totalPages = Math.ceil(data.totalHits / this.perPage);
-  data.error = null;
 
-  return q(this.cb(data));
-};
-
-ResponseHandler.prototype.failure = function() {
-  var error = JSON.parse(this.res.response.xhr.responseText);
-  var data = {
-    error: error,
-    page: this.page,
-    hits: [],
-    totalHits: null,
-    totalPages: null
-  };
-
-  return q(this.cb(data));
-};
-
-module.exports = ResponseHandler;
+export default ResponseHandler;
