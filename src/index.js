acbd27582dd9c9ff1572fe06d37d740452a6631d
Jacob <jacob@yola.com>
2017-02-24 13:17:31 -0800

    Enable overriding of the default url

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 'use strict';
 
 import assign from 'lodash.assign';
+import path from 'path';
+import url from 'url';
+
 import ResultList from './result-list';
 
 const requiredDefaults = {
   page: 1,
   per_page: 20,
+  url: 'https://pixabay.com/api'
 };
 
 const pixabayjs = {
   _auth: {},
 
-  _makeConfig: function(url, opts) {
-    const urlObj = { url };
-    return assign({}, requiredDefaults, urlObj, this.defaults, this._auth,
-      opts);
+  _makeConfig: function(endpoint, opts) {
+    const conf = assign({}, requiredDefaults, this.defaults, this._auth, opts);
+    const urlObj = url.parse(conf.url);
+
+    urlObj.pathname = path.join(urlObj.pathname, endpoint, '/');
+    conf.url = url.format(urlObj);
+
+    return conf;
   },
 
   defaults: {},
@@ -24,14 +32,12 @@ const pixabayjs = {
   },
 
   imageResultList: function(search, opts, onSuccess, onFailure) {
-    const url = 'https://pixabay.com/api';
-    const conf = this._makeConfig(url, opts);
+    const conf = this._makeConfig('', opts);
     return new ResultList(search, conf, onSuccess, onFailure);
   },
 
   videoResultList: function(search, opts, onSuccess, onFailure) {
-    const url = 'https://pixabay.com/api/videos';
-    const conf = this._makeConfig(url, opts);
+    const conf = this._makeConfig('videos', opts);
     return new ResultList(search, conf, onSuccess, onFailure);
   }
 };
